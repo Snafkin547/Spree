d718e4ebe6f06760b6d59610f5bb791e993eae29
ycd3 <2030416250@qq.com>
2021-10-17 13:01:06 -0400

    connectDB

--- project/backend/app.py
+++ project/backend/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, request
 import json
+
+import mysql.connector
+from flask import Flask, request
+
 from search.searchInput import searchInput
 
 # creates an instance of Flask app and pass it to the variable app
@@ -13,13 +16,24 @@ def home():
 # api address
 apiPrefix = '/api/v1'
 
+# database connector
+mydb = mysql.connector.connect(
+  host='localhost',
+  user='root',
+  password='',
+  database='CRUDDatabase',
+  #auth_plugin='mysql_native_password',
+  port=3306
+)
+mycur = mydb.cursor()
+
 # the function of search bar
 @app.route(apiPrefix + '/searchBar', methods=['POST'])
 def searchBar():
     keyword = request.get_data(as_text=True)
     print('get ' + keyword)
     res = {
-        'message': searchInput(keyword)
+        'message': searchInput(keyword[1:-1], mycur)
     }
     return json.dumps(res)
 
--- project/backend/search/searchInput.py
+++ project/backend/search/searchInput.py
@@ -1,3 +1,8 @@
-# There need a search algorithm
-def searchInput(key):
-    return "Pretend we get " + key
+# SearchBar results
+def searchInput(keyword, mycur):
+    mycur.execute("SELECT * FROM ITEM WHERE itemName LIKE '%%%s%%' OR desc_item LIKE '%%%s%%'"%(keyword, keyword))
+    res = mycur.fetchall()
+    if res:
+        res = map(lambda x: x[1]+' $'+x[2], res)
+        return 'We prepared '+', '.join(res)+' for you!'
+    return 'Sorry, we do not have what you want.'
