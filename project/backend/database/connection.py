ef664bdc5e64cd776cd1cf7e3e873bea39a536e6
Snafkin547 <harunobuishii547@gmail.com>
2021-11-08 20:59:49 -0500

    EN-023: mysql connector

--- project/backend/database/connection.py
+++ project/backend/database/connection.py
@@ -3,11 +3,11 @@ import mysql.connector
 class connection:
     def __init__(self):
 
-        mydb = mysql.connector.connect(
+        self.mydb = mysql.connector.connect(
             host='us-cdbr-east-04.cleardb.com',
             user='b1c819ea406612',
             password='35195fc1',
             database='heroku_993345239501248'
         )
-        return self.mydb.cursor(buffered=True)
+        self.cur=self.mydb.cursor(buffered=True)
 
