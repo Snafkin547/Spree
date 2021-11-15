f2c86928f139e578c0204648f09f78b65b3f0cd2
ana-malim <60043885+ana-malim@users.noreply.github.com>
2021-11-15 09:17:12 -0500

    EN-022: cleaning up script to create table

--- project/backend/cp_heroku_db.sql
+++ project/backend/cp_heroku_db.sql
@@ -1,9 +1,7 @@
 USE heroku_993345239501248;
 
-SET @@auto_increment_increment=1;
-
 CREATE TABLE cp_user (
-	user_id int(10) ZEROFILL AUTO_INCREMENT,
+	user_id int(10) ZEROFILL AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(128) NOT NULL,
 	password TEXT(128) NOT NULL,
     first_name VARCHAR(128),
@@ -12,50 +10,50 @@ CREATE TABLE cp_user (
     is_active CHAR(8) NOT NULL,
 	isSeller TINYINT,
     created_at timestamp,
-    PRIMARY KEY (user_id),
-    UNIQUE email_unique (email ASC) 
+    UNIQUE KEY email (email ASC) 
 );
 
 --User has address country/cities tables
+-- had to add ZEROFILL on the user_id to be exactly the same
 CREATE TABLE cp_user_address (
-	id int(10) ZEROFILL AUTO_INCREMENT,
-	user_id int,
+	id int(10) ZEROFILL AUTO_INCREMENT PRIMARY KEY,
+	user_id int(10) ZEROFILL,
 	address_line1 VARCHAR(128) NOT NULL,
     address_line2 VARCHAR(128),
     city VARCHAR(128) NOT NULL,
-    state VARCHAR(128) NOT NULL
+    state VARCHAR(128) NOT NULL,
     postal_code VARCHAR(128) NOT NULL,
     country VARCHAR(128) NOT NULL,
     phone_number int(10),
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES cp_user (user_id)
+    CONSTRAINT `fk_user_address`
+        FOREIGN KEY (user_id) REFERENCES cp_user (user_id)
 );
 
 -- PCI compliant
 CREATE TABLE cp_payment_details (
-	id int(10) ZEROFILL AUTO_INCREMENT,
-	user_id int,
+	id int(10) ZEROFILL AUTO_INCREMENT PRIMARY KEY,
+	user_id int ZEROFILL,
 	payment_type VARCHAR(128) NOT NULL,
     provider VARCHAR(128) NOT NULL,
     account_no int(10) NOT NULL,
     security_code int(5) NOT NULL,
-    expiry DATE(5) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES cp_user (user_id)
+    expiry DATE NOT NULL,
+    CONSTRAINT `fk_payment_user`
+        FOREIGN KEY (user_id) REFERENCES cp_user (user_id)
 );
 
-CREATE TABLE discount (
-	discount_id int NOT NULL AUTO_INCREMENT,
-    discount_code CHAR(50) NOT NULL UNIQUE,
+-- create a better one that can create the discount_code
+CREATE TABLE cp_discount (
+	discount_id int(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     discount_desc CHAR(128),
 	type VARCHAR(50),
 	value DECIMAL(19,4),
     expire_at timestamp,
-    PRIMARY KEY (discount_code)
+    discount_code CHAR(50) NOT NULL UNIQUE KEY
 ); 
 
 CREATE TABLE cp_product (
-	product_id int(8) NOT NULL AUTO_INCREMENT,
+	product_id int(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     product_init VARCHAR(50) NOT NULL,
 	name VARCHAR(128) NOT NULL,
     desc_item TEXT NOT NULL,
@@ -64,60 +62,58 @@ CREATE TABLE cp_product (
 	quantity INT NOT NULL,
 	img_src TEXT NOT NULL,
     created_at timestamp,
-    product_code VARCHAR(50) NOT NULL UNIQUE,
+    product_code CHAR(50) NOT NULL UNIQUE,
     discount_code CHAR(50),
-    PRIMARY KEY (product_id),
-    FOREIGN KEY (discount_code) REFERENCES discount (discount_code)
+    CONSTRAINT `fk_product_discount`
+        FOREIGN KEY (discount_code) REFERENCES discount (discount_code)
 ); 
 
 CREATE TABLE shopping_session (
-    id INT(30) NOT NULL AUTO_INCREMENT,
-    user_id INT,
+    id INT(30) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    user_id INT ZEROFILL,
     total DECIMAL(10) NOT NULL DEFAULT '0.00',
     created_at TIMESTAMP NOT NULL,
     modified_at TIMESTAMP,
-    PRIMARY KEY (`id`),
     CONSTRAINT `fk_shopping_user`
-        FOREIGN KEY (`user_id`) REFERENCES cp_user (`user_id `)
-       
+        FOREIGN KEY (user_id) REFERENCES cp_user (user_id)       
 );
 
 CREATE TABLE cart_item  (
-    id INT(8) NOT NULL AUTO_INCREMENT,
+    id INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     session_id INT,
     product_id INT,
     quantity DECIMAL(8) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     modified_at TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (session_id) REFERENCES shopping_session (id)
-    FOREIGN KEY (product_id) REFERENCES cp_product (product_id)   
+    CONSTRAINT `fk_cart_item_session`
+        FOREIGN KEY (session_id) REFERENCES shopping_session (id),
+    CONSTRAINT `fk_cart_item_product`
+        FOREIGN KEY (product_id) REFERENCES cp_product (product_id)
+);
+
+CREATE TABLE order_details (
+    id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT ZEROFILL,
+    payment_id INT ZEROFILL,
+    total DECIMAL(10) NOT NULL,    
+    created_at TIMESTAMP NOT NULL,
+    modified_at TIMESTAMP,
+    CONSTRAINT `fk_order_user`
+        FOREIGN KEY (user_id) REFERENCES cp_user (user_id),
+    CONSTRAINT `fk_order_payment`
+        FOREIGN KEY (payment_id) REFERENCES cp_payment_details (id)
 );
 
 CREATE TABLE order_items  (
-    id INT(8) NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     order_id INT,
     product_id INT,
     created_at TIMESTAMP NOT NULL,
     modified_at TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (order_id) REFERENCES order_details (id)
-    FOREIGN KEY (product_id) REFERENCES cp_product (product_id)   
+    CONSTRAINT `fk_order_items_details`
+        FOREIGN KEY (order_id) REFERENCES order_details (id),
+    CONSTRAINT `fk_order_items_product`
+        FOREIGN KEY (product_id) REFERENCES cp_product (product_id)   
 );
 
 
-CREATE TABLE order_details (
-    id INT(20) NOT NULL AUTO_INCREMENT,
-    user_id INT(10),
-    total DECIMAL(10) NOT NULL,
-    payment_id INT(20) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    modified_at TIMESTAMP,
-    UNIQUE KEY order_index (id),
-    UNIQUE KEY `customer_order_index` (`id`,`user_id`) USING BTREE,
-    PRIMARY KEY (id),
-    CONSTRAINT `fk_shopping_user_order`
-        FOREIGN KEY (user_id) REFERENCES cp_user (user_id)
-    CONSTRAINT `fk_order_payment`
-        FOREIGN KEY (payment_id) REFERENCES cp_payment_details (id)
-);
--- project/backend/insert_product.sql
+++ project/backend/insert_product.sql
@@ -3,11 +3,22 @@ AFTER INSERT ON product
 FOR EACH ROW
 SET new.product_code = CONCAT(product.product_init, product.product_id);
 
+
+-- Commands: verifies how the table was created to check if the user_id is a key
+-- erro code 1215 cannot add foreign key constraint
+SHOW ENGINE INNODB STATUS.
+SHOW CREATE TABLE cp_user;
+
+SET @auto_increment_increment=1;
+
 INSERT INTO product (`product_id`, `product_init`, `name`, `desc_item`, `category`,`price`, `quantity`, `img_src`, `created_at`) VALUES
-(1, 'APLWTC', 'Apple Watch', '', 'eletronics', '399', 10, 'apple-watch-s7-og-202110.jfif.jpg', '2021-11-8 17:55:22')
+(1, 'APLWTC', 'Apple Watch', '', 'eletronics', '399', 10, 'apple-watch-s7.jfif.jpg', '2021-11-8 17:55:22')
 
 UPDATE `heroku_993345239501248`.`user` SET `user_id` = '000001' WHERE (`user_id` = '000005');
 
 INSERT INTO discount (`discount_code`, `discount_desc`, `type`, `value`, `expire_at`) VALUES
 ('CSFP5', 'Take 5% OFF your first purchase!!', 'percentage', 5.0, ''),
 ('FreeShipping', 'Free shipping on all orders!!', '', '', '2021-12-01 23:59:59');
+
+column_virt INT(10) AS (column_1 + column_2) NOT NULL,
+KEY column_2_column_3_idx (column_2, column_3),
