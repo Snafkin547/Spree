9790b59143006d67a6d80554d2459f36e64e96a2
ana-malim <60043885+ana-malim@users.noreply.github.com>
2021-11-29 09:04:35 -0500

    releaseIter2: cleaning up code

--- project/backend/database.py
+++
@@ -1,5 +0,0 @@
-class Database:
-    host = 'us-cdbr-east-04.cleardb.com'
-    user = 'b1c819ea406612'
-    password = '35195fc1'
-    database = 'heroku_993345239501248'
--- project/frontend/src/pages/MainPage.js
+++ project/frontend/src/pages/MainPage.js
@@ -1,19 +1,6 @@
 import ProductList from '../components/ProductList';
 
 function MainPage() {
-
-  // state = {
-  //   modal: true
-  // };
-
-  // selectModal = info => {
-  //   this.setState({ modal: !this.state.modal }); // true/false toggle
-  // };
-    // const [initialData, setInitialData] = this.useState([{}]);
-
-    // useEffect(()=> {
-    //     fetch("/").then(response => response.json).then(data => setInitialData(data))
-    // }, []);
     return (
       <div className="App"> 
         <ProductList />
