97d76082ed472a2e55c95537f982574cfa3cda7a
jennyhopkins <jennyhopkins@alumni.upenn.edu>
2021-12-06 18:11:06 -0500

    EN-030 deliver to name connected to db

--- project/backend/checkout/checkoutInput.py
+++ project/backend/checkout/checkoutInput.py
@@ -7,7 +7,7 @@ def insertCheckoutInfoToDB(inputObject):
     my_cur = my_conn.cursor(buffered=True)
 
     #add all address fields
-    my_cur.execute("INSERT INTO cp_user_address (user_id, address_line1, address_line2, city, state, postal_code) VALUES (0000000025, '%s', '%s', '%s', '%s', '%s') "%(inputObject['address1'], inputObject['address2'], inputObject['city'], inputObject['state'], inputObject['zip']))
+    my_cur.execute("INSERT INTO cp_user_address (user_id, delivery_to, address_line1, address_line2, city, state, postal_code) VALUES (0000000025, '%s', '%s', '%s', '%s', '%s', '%s') "%(inputObject['name'], inputObject['address1'], inputObject['address2'], inputObject['city'], inputObject['state'], inputObject['zip']))
     
     #add all billing fields
     my_cur.execute("INSERT INTO cp_payment_details (user_id, name_on_card, card_number, exp_month, exp_year, bill_zipcode, security_code) VALUES (0000000025, '%s', '%s', '%s', '%s', '%s', '%s') "%(inputObject['nameCard'], inputObject['ccNumber'], inputObject['exp_month'], inputObject['exp_year'], inputObject['billingZip'], inputObject['secCode']))
