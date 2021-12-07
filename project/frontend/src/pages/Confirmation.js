db88eff5869b003b2bb0a67620ad482d6a9824db
jennyhopkins <jennyhopkins@alumni.upenn.edu>
2021-12-07 00:18:52 -0500

    added confirmation page

---
+++ project/frontend/src/pages/Confirmation.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Confirmation = () => {
+    return (
+            <h1>Thank you for your order! Order #123 has been received and a confirmation will be emailed to you shortly.</h1>
+    );
+};
+export default Confirmation;
