385bb8f4aa0f0dce3f00b106ec2c781461cba96d
ana-malim <60043885+ana-malim@users.noreply.github.com>
2021-12-07 19:00:18 -0500

    main: cleaning build

--- project/frontend/src/pages/Search.js
+++ project/frontend/src/pages/Search.js
@@ -3,7 +3,6 @@ import SearchBar from '../components/Search';
 function Search() {
     return (
         <div className="App">
-            it does not work.
             <h4>{SearchBar.searchResult}</h4>
         </div>
     );
